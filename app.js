df3b746daeb8e6906d57de40130ba02136b7258d
Sulaiman Albulaihy <33493055+Sulaiman122@users.noreply.github.com>
2021-12-14 12:06:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,13 +8,7 @@ require('./config/passport')(passport);
 
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://w09d05.herokuapp.com", 
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.urlencoded({ extended: false }))
 app.use(
